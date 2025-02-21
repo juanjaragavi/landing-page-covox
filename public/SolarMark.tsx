bddefcaf6b9f4d0983ef8d94bf640ce0ba8dff7a
Juan Jaramillo <info@juanjaramillo.tech>
2025-02-21 07:56:20 -0500

    Actualizar atributos SVG en el componente SolarMark para seguir las convenciones de JSX.

--- public/SolarMark.tsx
+++ public/SolarMark.tsx
@@ -7,17 +7,17 @@ export const SolarMark = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
     <g
       id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     ></g>
     <g id="SVGRepo_iconCarrier">
       {" "}
       <path
         d="M0 14.545L1.455 16 8 9.455 14.545 16 16 14.545 9.455 8 16 1.455 14.545 0 8 6.545 1.455 0 0 1.455 6.545 8z"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       ></path>{" "}
     </g>
   </svg>
