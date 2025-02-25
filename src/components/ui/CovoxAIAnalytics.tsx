07db2cb2dd9375112f373a6f4fa04b070bbd50d1
Juan Jaramillo <info@juanjaramillo.tech>
2025-02-24 20:40:36 -0500

    Links updated leading to the Login Backend

--- src/components/ui/CovoxAIAnalytics.tsx
+++ src/components/ui/CovoxAIAnalytics.tsx
@@ -67,7 +67,7 @@ export function CovoxAIAnalytics() {
         <div className="mt-8 flex justify-center md:col-span-10">
           <a
             className="inline-flex cursor-pointer flex-row items-center justify-center gap-1 rounded-xl border-b-[1.5px] border-[#77FF00] bg-[#77FF00] px-6 py-4 text-lg leading-4 font-semibold tracking-wide whitespace-nowrap text-black shadow-[0_0_0_2px_rgba(0,0,0,0.04),0_0_10px_0_rgba(255,255,255,0.15)] transition-all duration-200 ease-in-out hover:bg-white hover:shadow-white/50"
-            href="#"
+            href="https://admin.covox.io/login"
           >
             Empieza ahora con COVOX AI 🚀
           </a>
--- src/components/ui/Features.tsx
+++ src/components/ui/Features.tsx
@@ -430,7 +430,7 @@ export default function Features() {
         <FadeDiv className="absolute left-1/2 z-50 flex -translate-x-1/2 items-center justify-center">
           <a
             className="inline-flex cursor-pointer flex-row items-center justify-center gap-1 rounded-xl border-b-[1.5px] border-[#77FF00] bg-[#77FF00] px-6 py-4 text-lg leading-4 font-semibold tracking-wide whitespace-nowrap text-black shadow-[0_0_0_2px_rgba(0,0,0,0.04),0_0_10px_0_rgba(255,255,255,0.15)] transition-all duration-200 ease-in-out hover:bg-white hover:shadow-white/50 sm:text-xl"
-            href="#"
+            href="https://admin.covox.io/login"
           >
             Empieza ahora con COVOX AI 🚀
           </a>
