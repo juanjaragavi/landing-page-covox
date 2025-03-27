1bb62f1bf28e7c7e2e9170395f7f26471c922de1
Jhonnatan <jacosta.co@gmail.com>
2025-03-27 10:30:47 -0500

    Ajustes de pixel de fb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { GeistSans } from "geist/font/sans"
 import type { Metadata } from "next"
+import Script from "next/script"
 import { lazy, Suspense } from "react"
 import "./globals.css"
 import { siteConfig } from "./siteConfig"
@@ -121,6 +122,38 @@ export default function RootLayout({
             html.dark { color-scheme: dark !important; }
           }
         `}</style>
+        {/* Google Tag Manager - Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-RD33RKF5HM');
+            `,
+          }}
+        />
+        <Script
+          id="pixel-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1293950901708265');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${GeistSans.className} min-h-[100dvh] overflow-x-hidden scroll-auto bg-gray-900 antialiased selection:bg-[#77FF00] selection:text-gray-800`}
